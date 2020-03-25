963045a10b40264a346149e51659140f79eb1ce0
benbucklin <5721984+benbucklin@users.noreply.github.com>
2020-03-24 20:32:28 -0400

    Update with comments from meeting

--- index.js
+++ index.js
@@ -23,5 +23,19 @@ port.on("open", () => {
 // Callback on serial port data
 parser.on('data', data =>{
     console.log(data);
-    port.write("TODO: Figure out input");
+
+    // TODO: Move this to a button onClick, where the value is updated from UI
+    port.write("OXYGEN = 20");
 });
+
+// UI --> ARDUINO (ASYNC)
+// OXYGEN LEVEL: 20 - 100
+// PEAK EXPIRATION PRESSURE: 0 - 30
+// PEAK INSPIRATION PRESSURE: 0 - 30
+// PEAK EXPIRATION <= PEAK INSPIRATION
+
+// ARDUINO --> UI (SYNC @ 1 HZ ?)
+// PRESSURE: 0 - 30
+// FLOW: 0 - X
+// VOLUME: 0 - X
+// NOTIFICATIONS: UPDATES
