65c3b9a75899cd6885cc4ef4a493d7243bb1127e
Omar Husain <omar.husain2@gmail.com>
2021-02-25 21:14:24 -0500

    3 sensors connected by SPI and control valve is wired

--- arduino/controller/spi_sensor_test.ino
+++ arduino/controller/spi_sensor_test.ino
@@ -2,6 +2,8 @@
 
 const int inletPin = 10;
 const int p1_pin   = 9;
+const int p2_pin   = 2;
+const int valvePin = 5;
 
 #define SPI_CLOCK_SPEED 800000 // hz
 
@@ -16,14 +18,22 @@ void setup() {
   // Setup serial
   Serial.begin(57600);
 
-  // Set slave pins as OUTPUT (when slave is HIGH it ignores master, when it is LOW it communicates)
+  // Setup control pin for valve
+  pinMode(valvePin, OUTPUT);
+
+  // Set slaveSelectPin as OUTPUT (when slave is HIGH it ignores master, when it is LOW it communicates
   pinMode(inletPin, OUTPUT);
   pinMode(p1_pin, OUTPUT);
+  pinMode(p2_pin, OUTPUT);
 
   // De-assert slave pins
   digitalWrite(inletPin, HIGH);
   digitalWrite(p1_pin, HIGH);
+  digitalWrite(p2_pin, HIGH);
 
+  // reset sensors
+  reset_sensors();
+  
   delay(1);
 
   // Initialize SPI
@@ -33,39 +43,67 @@ void setup() {
 
 void loop() {
 
-
-  // Get pressure readings
+  // Get inlet pressure reading
   double p_inlet = read_pressure(inletPin);
   double p1      = read_pressure(p1_pin);
+  double p2      = read_pressure(p2_pin);
 
-  // print preasure readings
+  // Print pressure readings
   print_pressure_reading(p_inlet);
   print_pressure_reading(p1);
+  print_pressure_reading(p2);
+
+  // Open and close the valve
+  digitalWrite(valvePin, HIGH);
+  Serial.println("opened valve");
+  delay(2000);
+  digitalWrite(valvePin, LOW);
+  Serial.println("closed valve");
+  delay(2000);
 
  
 }
 
-/* Function to initiate any SPI sequence to a given slave device
- * Clock speed, MSB, SPI_MODE all given in sensor datasheet.
- */
+/* Call before any spi transfer sequence */
 void initiate_SPI_transfer(int slave_pin)
 {
   SPI.beginTransaction(SPISettings(SPI_CLOCK_SPEED, MSBFIRST, SPI_MODE0));
   digitalWrite(slave_pin, LOW);
 }
 
-/* Handles reading pressure count from given sensor
- * Uses SPI based on datasheet commands
- * Sensor sends back 24-bit pressure count
- * Use transfer function A from datasheet to conver to PSI 
- */
+/* Handles SPI transfer between pressure sensor at the slave_pin and master. */
 double read_pressure(int slave_pin)
 {
 
+  // Step 1 and 2: Check pressure sensor status
+  uint8_t stat = connect_to_sensor(slave_pin);
+
+  // Step 3: Get pressure reading [24 bit count] from sensor
+  uint32_t reading = 0;
+  for(uint8_t i = 0; i<3; i++)
+  {
+    reading |= SPI.transfer(0x00);
+    if (i != 2) reading = reading << 8;
+  }
+
+  // De-select pin and end transaction
+  digitalWrite(slave_pin, HIGH);
+  SPI.endTransaction();
+
+  // Wait to clear bus before reading next sensor
+  delay(5);
+
+   // convert to pressure with transfer function
+  return ((reading - OUTPUT_MIN)*(P_MAX - P_MIN))/(OUTPUT_MAX - OUTPUT_MIN) + P_MIN;
+   
+}
+
+uint8_t connect_to_sensor(int slave_pin)
+{
   // Start SPI transaction with pin
   initiate_SPI_transfer(slave_pin);
 
-  // Step 1 From datasheet pp 18
+  // Step 1 - Datasheet SPI commands pp 18
   SPI.transfer(0xAA);
   SPI.transfer(0x00);
   SPI.transfer(0x00);
@@ -79,38 +117,34 @@ double read_pressure(int slave_pin)
 
   // Step 3
   initiate_SPI_transfer(slave_pin);
-  
-
-  uint8_t stat     = SPI.transfer(0xF0);
+  uint8_t stat = SPI.transfer(0xF0);
   Serial.print("status = ");
   Serial.println(stat);
 
-  // Get pressure count reading [3x8bits = 24 bit reading]
-  uint32_t reading = 0;
-  for(uint8_t i = 0; i<3; i++)
-  {
-    reading |= SPI.transfer(0x00);
-    if (i != 2) reading = reading << 8;
-    Serial.println(reading);
-  }
-
-  // De-select pin and end transaction
-  digitalWrite(slave_pin, HIGH);
-  SPI.endTransaction();
-
-  // Wait to clear bus before reading next sensor
-  delay(10);
-
-   // convert to pressure with transfer function
-  return ((reading - OUTPUT_MIN)*(P_MAX - P_MIN))/(OUTPUT_MAX - OUTPUT_MIN) + P_MIN;
-   
+  return stat;
 }
 
-/* Prints pressure reading to serial in a nice way */
 void print_pressure_reading(double pressure)
 {
 
   Serial.print("pressure is ");
   Serial.print(pressure);
   Serial.println("PSI");
+}
+
+void reset_sensors(void)
+{
+  // Assert then de-assert pins to clear out any junk data
+  digitalWrite(inletPin, LOW);
+  digitalWrite(inletPin, HIGH);
+  
+  delay(5);
+  
+  digitalWrite(p1_pin, LOW);
+  digitalWrite(p1_pin, HIGH);
+
+  delay(5);
+  
+  digitalWrite(p2_pin, LOW);
+  digitalWrite(p2_pin, HIGH);
 }
